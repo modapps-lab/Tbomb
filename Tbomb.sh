ef30b8ff9819889a5700ceccf72f92b9907c5a98
modapps-lab <appsmod374@gmail.com>
2020-07-25 14:48:08 +0530

    first commit

---
+++ Tbomb.sh
@@ -0,0 +1,11 @@
+pkg update
+pkg upgrade
+apt-get update
+apt-get upgrade -y
+pkg install git
+pkg install python
+git clone https://github.com/TheSpeedX/TBomb.git
+cd TBomb
+chmod +x TBomb.sh
+./TBomb.sh
+
