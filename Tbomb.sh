85e9fcd5aa2ace355e6d6a6aa98f1b8e25f5b5a4
modapps-lab <68771906+modapps-lab@users.noreply.github.com>
2020-07-26 20:34:53 +0530

    Update Tbomb.sh

--- Tbomb.sh
+++ Tbomb.sh
@@ -1,11 +1,18 @@
 pkg update
-pkg upgrade
+pkg upgrade -y
 apt-get update
 apt-get upgrade -y
-pkg install git
-pkg install python
+pkg install git -y
+pkg install python -y
 git clone https://github.com/TheSpeedX/TBomb.git
+clear
+echo "///////SUBSCRIBE Mod Apps\\\\\\\\"
+echo '///////THIS IS ONLY 1 TIME BASH URL\\\\\\"
+echo "///////NEXT TIME USE\\\\\\\"
+echo "THIS COMMAND TO START"
+echo "cd TBomb"
+echo "bash TBomb.sh"
+sleep 1
 cd TBomb
 chmod +x TBomb.sh
 ./TBomb.sh
-
