cb2d60187d71df16bee49bdc9b18dca055b13f44
modapps-lab <68771906+modapps-lab@users.noreply.github.com>
2020-07-27 14:33:53 +0530

    Update Tbomb.sh

--- Tbomb.sh
+++ Tbomb.sh
@@ -4,14 +4,21 @@ apt-get update -y
 apt-get upgrade -y
 pkg install git -y
 pkg install python -y
+pkg install toilet -y
+pkg install pv -y
 git clone https://github.com/TheSpeedX/TBomb.git
 clear
-echo "{SUBSCRIBE Mod Apps}"
-echo "{THIS IS ONLY 1 TIME BASH URL}"
-echo "{NEXT TIME USE}"
-echo "{THIS COMMAND TO START}"
-echo "(cd TBomb)"
-echo "(bash TBomb.sh)"
+toilet "{SUBSCRIBE Mod Apps}" -f term -F border --gay | pv -qL 100	
+sleep 3
+toilet "{THIS IS ONLY 1 TIME BASH URL}" -f term -F border --gay | pv -qL 100	
+sleep 3
+toilet "{NEXT TIME USE}" -f term -F border --gay | pv -qL 100	
+sleep 2
+toilet "{THIS COMMAND TO START}" -f term -F border --gay | pv -qL 100	
+sleep 2
+toilet "cd TBomb" -f term -F border --gay | pv -qL 100	
+sleep 1
+toilet "./TBomb.sh" -f term -F border --gay | pv -qL 100	
 sleep 10
 cd TBomb
 chmod +x TBomb.sh
